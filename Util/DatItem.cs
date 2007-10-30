a81fe408d7490318c68ca8bb6f6d065f3ffc3111
ame <ame@ad62ca58-583e-0410-b638-772cefa7fa72>
2007-10-30 01:16:27 +0000

    Did the long and boring part of the DatReader... Its not done yet.. but its a start.. so we dont start on different stuff

---
+++ Util/DatItem.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tibia
+{
+    public class DatItem
+    {
+        public bool IsContainer;
+        public uint ReadWriteInfo;
+        public bool IsFluidContainer;
+        public bool IsStackable;
+        public bool MultiType;
+        public bool Useable;
+        public bool IsNotMovable;
+        public bool AlwaysOnTop;
+        public bool IsGroundTile;
+        public bool IsPickupAble;
+        public bool Blocking;
+        public bool BlockPickupable;
+        public bool IsWalkable;
+        public bool IsDoor;
+        public bool IsDoorWithLock;
+        public byte Speed;
+        public bool CanDecay;
+        public bool HasExtraByte;
+        public bool IsField;
+        public bool IsDepot;
+        public bool MoreAlwaysOnTop;
+        public bool Useable2;
+    }
+}
