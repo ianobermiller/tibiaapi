fda82ab5f6404d62e8694c9bc0c033bd769d0465
ian320 <ian320@ad62ca58-583e-0410-b638-772cefa7fa72>
2008-04-09 15:01:36 +0000

    Added: Skeleton for XTEA encryption/decryption.

---
+++ tibiaapi/Util/XTEA.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Tibia.Util
+{
+    /// <summary>
+    /// Contains functions for decrypting/encrypting packets
+    /// using XTEA encryption
+    /// </summary>
+    public static class XTEA
+    {
+        /// <summary>
+        /// Decrypte a packet using XTEA.
+        /// </summary>
+        /// <param name="packet"></param>
+        /// <param name="key"></param>
+        public static void Decrypt(byte[] packet, byte[] key)
+        {
+            
+        }
+
+        /// <summary>
+        /// Encrypt a packet using XTEA.
+        /// </summary>
+        /// <param name="packet"></param>
+        /// <param name="key"></param>
+        public static void Encrypt(byte[] packet, byte[] key)
+        {
+
+        }
+    }
+}
