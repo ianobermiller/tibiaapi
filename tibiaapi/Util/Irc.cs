69b6933b8dac9a5a61ade2d86761a646b73dc4a3
hugo.persson <hugo.persson@ad62ca58-583e-0410-b638-772cefa7fa72>
2008-05-12 14:54:46 +0000

    Added MessageType, with parser
    Still very basic, atleast now its possible to say stuff!

--- tibiaapi/Util/Irc.cs
+++ tibiaapi/Util/Irc.cs
@@ -22,8 +22,11 @@ namespace Tibia.Util
         byte[] readBuffer = new byte[8096];
         Queue<string[]> messageQueue = new Queue<string[]>();
 
-        public delegate void CommandListener(string[] command);
+        public delegate void CommandListener(IrcMessage message);
         public CommandListener ReceivedCommand;
+
+        public delegate void MessageListener(IrcMessage message);
+        public MessageListener ReceivedMessage;
 
         #region Constructor
         public Irc(string user, string server)
@@ -91,8 +94,7 @@ namespace Tibia.Util
             if (bytesRead > 0)
             {
                 string command = System.Text.Encoding.GetEncoding("iso-8859-1").GetString(readBuffer, 0, bytesRead);
-                string[] commandParts = command.Split(' ');
-
+                string[] commandParts = command.Split('\n');
                 messageQueue.Enqueue(commandParts);
             }
 
@@ -106,18 +108,22 @@ namespace Tibia.Util
             if (messageQueue.Count > 0)
             {
                 string[] commandParts = messageQueue.Dequeue();
-
-                if (ReceivedCommand != null) ReceivedCommand(commandParts);
-
-                if (commandParts[0].Substring(0, 1) == ":")
+                for (int i = 0; i < commandParts.Length - 1; i++)
                 {
-                    commandParts[0] = commandParts[0].Remove(0, 1);
-                }
-
-                if (commandParts[0] == "PING")
-                {
-                    // Server PING, send PONG back
-                    Ping(commandParts);
+                    IrcMessage message = new IrcMessage(commandParts[i]);
+                    if (ReceivedCommand != null) ReceivedCommand(message);
+                    if (commandParts[0].Length > 0)
+                    {
+                        switch (message.Type)
+                        {
+                            case MessageType.PING:
+                                Ping(message);
+                                break;
+                            case MessageType.PRIVMSG:
+                                ReceivedMessage(message);
+                                break;
+                        }
+                    }
                 }
             }
 
@@ -126,17 +132,119 @@ namespace Tibia.Util
                 ProcessMessageQueue();
             }
         }
-        #region Ping
-        private void Ping(string[] command)
+        public void Say(string Message)
         {
+            writer.WriteLine(MessageType.PRIVMSG.ToString() +" "+ this.Channel + " :" + Message);
+            writer.Flush();
+        }
+        #region Ping
+        private void Ping(IrcMessage message)
+        {
+            string[] data = message.RawData.Split(' ');
             string pingHash = "";
-            for (int intI = 1; intI < command.Length; intI++)
+            for (int intI = 1; intI < data.Length; intI++)
             {
-                pingHash += command[intI] + " ";
+                pingHash += data[intI] + " ";
             }
             writer.WriteLine("PONG " + pingHash);
             writer.Flush();
         }
         #endregion
+    }
+    public class IrcMessage
+    {
+        private string from;
+        private string to;
+        private string message;
+        private string rawdata;
+        private MessageType msgtype;
+
+        public string From
+        {
+            get { return this.from; }
+            set { this.from = value; }
+        }
+
+        public string To
+        {
+            get { return this.to; }
+            set { this.to = value; }
+        }
+
+        public string Message
+        {
+            get { return this.message; }
+            set { this.message = value; }
+        }
+
+        public string RawData
+        {
+            get { return this.rawdata; }
+            set { this.rawdata = value; }
+        }
+
+        public MessageType Type
+        {
+            get { return this.msgtype; }
+            set { this.msgtype = value; }
+        }
+
+        public IrcMessage(string Message)
+        {
+            this.rawdata = Message;
+            Parse(Message);
+        }
+
+        public IrcMessage()
+        {
+
+        }
+
+        public IrcMessage Parse(string Data)
+        {
+            IrcMessage result = new IrcMessage(); ;
+            string line;
+            string[] lines;
+
+            int exppos;
+            int colpos;
+            if (Data[0] == ':')
+            {
+                line = Data.Substring(1);
+            }
+            else
+            {
+                line = Data;
+            }
+            lines = line.Split(new char[] { ' ' });
+            try
+            {
+                result.msgtype = (MessageType)Enum.Parse(typeof(MessageType), lines[1], true);
+            }
+            catch (Exception e)
+            {
+                result.msgtype = MessageType.NULL;
+            }
+                exppos = lines[0].IndexOf("!");
+            colpos = line.IndexOf(" :");
+            to = lines[2];
+            if (colpos != -1)
+            {
+                this.message = line.Substring(colpos + 2);
+                this.message = this.message.Replace("\r\n", "");
+            }
+            if (exppos != -1)
+            {
+                this.from = lines[0].Substring(0, exppos);
+            }
+            return null;
+        }
+    }
+    public enum MessageType
+    {
+        PRIVMSG,
+        NOTICE,
+        PING,
+        NULL
     }
 }
