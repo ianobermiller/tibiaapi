b7956a670787f8a7395de6d5208add6187c79e91
Oskari.Virtanen <Oskari.Virtanen@ad62ca58-583e-0410-b638-772cefa7fa72>
2008-07-08 23:28:03 +0000

    Updated almost rest of the addresses. ONLY PLAYER_TILE ADDRESS IS OLD! (Requesting reinforcements to update it!). Also there's few minor addresses without updating (they're not used in rest of the code).
    Also fixed a bug in Battlelist addresses. (Battlelist.End was same as Battlelist.Start).

--- tibiaapi/Addresses/Hotkey.cs
+++ tibiaapi/Addresses/Hotkey.cs
@@ -2,16 +2,16 @@ namespace Tibia.Addresses
 {
     public class Hotkey
     {
-        public static uint SendAutomaticallyStart = 0x769298; //8.11
+        public static uint SendAutomaticallyStart = 0x7773C8;
         public static uint SendAutomaticallyStep = 0x1;
 
-        public static uint TextStart = 0x7692C0; //8.11
+        public static uint TextStart = 0x7773F0;
         public static uint TextStep = 0x100;
 
-        public static uint ObjectStart = 0x769208; //8.11
+        public static uint ObjectStart = 0x777338;
         public static uint ObjectStep = 0x4;
 
-        public static uint ObjectUseTypeStart = 0x7690E8; //8.11
+        public static uint ObjectUseTypeStart = 0x777218;
         public static uint ObjectUseTypeStep = 0x4;
 
         public static uint MaxHotkeys = 36;
