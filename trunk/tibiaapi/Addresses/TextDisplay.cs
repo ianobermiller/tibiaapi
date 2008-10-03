128c8fccba1251b87d9fdf6c4a71b524d88e1d5a
marcsalvat <marcsalvat@ad62ca58-583e-0410-b638-772cefa7fa72>
2008-10-03 01:35:55 +0000

    updated textdisplay adresses.

--- trunk/tibiaapi/Addresses/TextDisplay.cs
+++ trunk/tibiaapi/Addresses/TextDisplay.cs
@@ -5,30 +5,30 @@
         /// <summary>
         /// PrintName function call to overwrite
         /// </summary>
-        public static int PrintName = 0x4EC5E1; // 8.22
+        public static int PrintName = 0x4EF0F1; // 8.31
 
 
         /// <summary>
         /// PrintFPS function call to overwrite
         /// </summary>
-        public static int PrintFPS = 0x4563C8; // 8.22
+        public static int PrintFPS = 0x4585E8; // 8.31
 
 
         /// <summary>
         /// Offset, if user wants to show FPS
         /// </summary>
-        public static int ShowFPS = 0x624974; // 8.22
+        public static int ShowFPS = 0x6289F4; // 8.31
 
         /// <summary>
         /// PrintText function address
         /// </summary
-        public static int PrintTextFunc = 0x4AC490; // 8.22
+        public static int PrintTextFunc = 0x4AEB90; // 8.31
 
 
         /// <summary>
         /// Conditional check to show FPS
         /// </summary>
-        public static int NopFPS = 0x456304; // 8.22
+        public static int NopFPS = 0x458524; // 8.31
 
     }
 }
