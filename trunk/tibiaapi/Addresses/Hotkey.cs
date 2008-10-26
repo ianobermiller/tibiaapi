60af47fb189502ca70e4b59e88c125368f36441d
marcsalvat <marcsalvat@ad62ca58-583e-0410-b638-772cefa7fa72>
2008-10-26 18:57:33 +0000

    -Fixed IntenseHealingRune -Fixed ObjectStart Adress -Added a small delay when returning Rune done to BP (i think it didnt work bcause of the packet.dll problem but now it works fine)

--- trunk/tibiaapi/Addresses/Hotkey.cs
+++ trunk/tibiaapi/Addresses/Hotkey.cs
@@ -8,7 +8,7 @@ namespace Tibia.Addresses
         public static uint TextStart = 0x780498;// 8.31
         public static uint TextStep = 0x100;
 
-        public static uint ObjectStart = 0x780350;// 8.31
+        public static uint ObjectStart = 0x7803E0;// 8.31
         public static uint ObjectStep = 0x4;
 
         public static uint ObjectUseTypeStart = 0x7802C0;// 8.31
