15f41bfaf085f1bb3f5e0fc1a34eae51876eec3d
joebingham07 <joebingham07@ad62ca58-583e-0410-b638-772cefa7fa72>
2008-11-19 10:28:59 +0000

    FollowPacket - Sends a follow packet to the server using the targets ID. NPCTradeBuyItemPacket - Sends a packet to the server to buy an item from an NPC. I only tested this with the trade list open, and I would suggest only sending this packet in that way. NPCTradeSellItemPacket - Sends a packet to the server to sell an item to an NPC. I only tested this with the trade list open, and I would suggest only sending this packet in that way.

---
+++ trunk/tibiaapi/Packets/Outgoing/FollowPacket.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Tibia.Objects;
+
+namespace Tibia.Packets
+{
+    /// <summary>
+    /// Packet sent to the server to indicate which creature is to be followed.
+    /// </summary>
+    public class FollowPacket : Packet
+    {
+        int id;
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public FollowPacket(Client c)
+            : base(c)
+        {
+            type = PacketType.FlagUpdate;
+            destination = PacketDestination.Server;
+        }
+
+        public FollowPacket(Client c, byte[] data)
+            : this(c)
+        {
+            ParseData(data);
+        }
+
+        public new bool ParseData(byte[] packet)
+        {
+            if (base.ParseData(packet))
+            {
+                if (type != PacketType.FlagUpdate) return false;
+                PacketBuilder p = new PacketBuilder(client, packet, 3);
+                id = p.GetLong();
+                index = p.Index;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static FollowPacket Create(Client c, int id)
+        {
+            PacketBuilder p = new PacketBuilder(c, PacketType.FlagUpdate);
+            p.AddLong(id);
+            return new FollowPacket(c, p.GetPacket());
+        }
+    }
+}
