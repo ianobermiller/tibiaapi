6e03beacdaf692dc21561d97769e5ccbf4a205d9
brunodunbar <brunodunbar@ad62ca58-583e-0410-b638-772cefa7fa72>
2008-12-23 14:57:11 +0000

    Added FightModesPacket.

---
+++ tibiaapi/Packets/Outgoing/FightModesPacket.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tibia.Packets.Outgoing
+{
+    public class FightModesPacket : OutgoingPacket
+    {
+        public byte FightMode { get; set; }
+        public byte ChaseMode { get; set; }
+        public byte SafeMode { get; set; }
+
+        public FightModesPacket(Objects.Client c)
+            : base(c)
+        {
+            Type = OutgoingPacketType.FightModes;
+            Destination = PacketDestination.Server;
+        }
+
+        public override bool ParseMessage(NetworkMessage msg, PacketDestination destination, ref Objects.Location pos)
+        {
+            if (msg.GetByte() != (byte)OutgoingPacketType.FightModes)
+                return false;
+
+            Destination = destination;
+            Type = OutgoingPacketType.FightModes;
+
+            FightMode = msg.GetByte();
+            ChaseMode = msg.GetByte();
+            SafeMode = msg.GetByte();
+
+            return true;
+        }
+
+        public override byte[] ToByteArray()
+        {
+            NetworkMessage msg = new NetworkMessage(Client, 0);
+            msg.AddByte((byte)Type);
+
+            msg.AddByte(FightMode);
+            msg.AddByte(ChaseMode);
+            msg.AddByte(SafeMode);
+
+            return msg.Packet;
+        }
+
+        public static bool Send(Objects.Client client, byte fightMode, byte chaseMode, byte safeMode)
+        {
+            FightModesPacket p = new FightModesPacket(client);
+            p.FightMode = fightMode;
+            p.ChaseMode = chaseMode;
+            p.SafeMode = safeMode;
+            return p.Send();
+        }
+    }
+}
