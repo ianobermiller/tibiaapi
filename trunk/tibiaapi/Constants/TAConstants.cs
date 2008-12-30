748605463f9abec502cc7fad8c1d0d3f3fae57ee
ian320 <ian320@ad62ca58-583e-0410-b638-772cefa7fa72>
2008-12-30 17:34:17 +0000

    Added TAConstants.cs file with global TibiaAPI constants, such as the AppDataPath.

---
+++ trunk/tibiaapi/Constants/TAConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tibia.Constants
+{
+    public static class TAConstants
+    {
+        public static string AppDataPath = System.IO.Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TibiaAPI");
+    }
+}
