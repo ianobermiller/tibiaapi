47a6f95216fd4554c49a9fc38ae53ac3625fff70
geancarlo.rocha@gmail.com <geancarlo.rocha@gmail.com@ad62ca58-583e-0410-b638-772cefa7fa72>
2008-12-30 17:01:30 +0000

    Added characters list acquiring.

--- tibiaapi/Clientless/Connection.cs
+++ tibiaapi/Clientless/Connection.cs
@@ -69,7 +69,34 @@ namespace Tibia.Clientless
         private uint[] xteaKey;
         private bool isConnected;*/
 
+        public delegate void Notification(string message);
 
+        public Notification Socket_Connected;
+        public Notification Socket_Disconnected;
+
+        public Notification LoginServer_CharList;
+        public Notification LoginServer_OnError;
+        public Notification LoginServer_FYI;
+        public Notification LoginServer_MOTD;
+        public Notification LoginServer_Patching;
+        public Notification LoginServer_SelectAnother;
+        public Notification LoginServer_UnknownMsg;
+        public Notification LoginServer_CouldNotConnect;
+        public Notification LoginServer_ReceivedNothing;
+
+        #region Properties
+        public CharList[] CharList
+        {
+            get { return charList; }
+        }
+
+        public uint[] XteaKey
+        {
+            get { return xteaKey.ToUInt32Array(); }
+        }
+        #endregion
+
+        #region Constructors/Destructors
         public Connection(OperationalSystem OpSystem, ushort Version, string AccountName, string Password, bool OpenTibia, bool Debug) :
             this(OpSystem,Version, AccountName, Password, OpenTibia, loginServers, Debug) { }
 
@@ -84,6 +111,7 @@ namespace Tibia.Clientless
             MaxLoginServers = ls.Length;
             OS = (byte)OpSystem;
         }
+        #endregion
 
         public void GetCharacters(bool RetryIfError)
         {
@@ -104,9 +132,12 @@ namespace Tibia.Clientless
             try
             {
                 socket.EndConnect(ar);
+                xteaKey = new byte[16];
                 rand.NextBytes(xteaKey);
-                socket.Send(LoginServerRequestPacket.CreateLoginServerRequestPacket(version, Signatures.Tibia840,
-                    AccName, Password).Packet);
+                if (Socket_Connected != null)
+                    Socket_Connected("Login Server");
+                socket.Send(LoginServerRequestPacket.CreateLoginServerRequestPacket(OS,version, Signatures.Tibia840,
+                    xteaKey,AccName, Password).Packet);
                 socket.BeginReceive(dataLoginServer, 0, dataLoginServer.Length, SocketFlags.None, (AsyncCallback)LoginServerReceived, null);
             }
             catch(Exception ex)
@@ -125,37 +156,51 @@ namespace Tibia.Clientless
                     byte[] tmp = new byte[dataLength];
                     Array.Copy(dataLoginServer, tmp, dataLength);
                     NetworkMessage msg = new NetworkMessage(tmp);
-                    msg.PrepareToRead();
+                    msg.PrepareToRead(xteaKey.ToUInt32Array());
                     msg.GetUInt16(); 
                     while (msg.Position < msg.Length)
                     {
                         byte cmd = msg.GetByte();
-
+                        string message;
                         switch (cmd)
                         {
                             case 0x0A: //Error message
-                                /*Notification*/
-                                msg.GetString();
+                                message=msg.GetString();
+                                if (LoginServer_OnError != null)
+                                    LoginServer_OnError(message);
                                 break;
                             case 0x0B: //For your information
-                                /*Notification*/
-                                msg.GetString();
+                                message = msg.GetString();
+                                if (LoginServer_FYI != null)
+                                    LoginServer_FYI(message);
                                 break;
                             case 0x14: //MOTD
-                                /*Notification*/
-                                msg.GetString();
+                                message = msg.GetString();
+                                if (LoginServer_MOTD != null)
+                                    LoginServer_MOTD(message);
                                 break;
                             case 0x1E: //Patching exe/dat/spr messages
                             case 0x1F:
                             case 0x20:
-                                /*Notification  "A new client is avalible, please download it first!"*/
+                                if (LoginServer_Patching != null)
+                                    LoginServer_Patching("A new client is available.");
                                 return;
-                            case 0x28: //Select other login server
+                            case 0x28: //Select another login server   
+                                if (LoginServer_SelectAnother != null)
+                                    LoginServer_SelectAnother("Select another login server.");
                                 if (retry)
                                 {
                                     if (LoginServerIndex < MaxLoginServers - 1)
                                         TryLoginServer(LoginServerIndex++);
-                                    else { }//tried all login servers and couldnt get character list, throw a notification
+                                    else 
+                                    {
+                                        if (LoginServer_CouldNotConnect != null)
+                                            LoginServer_CouldNotConnect("Select another login server.");
+
+                                        socket.Disconnect(true);
+                                        if (Socket_Disconnected != null)
+                                            Socket_Disconnected("dataLength<=0");
+                                    }
                                 }
                                 break;
                             case 0x64: //character list
@@ -167,37 +212,65 @@ namespace Tibia.Clientless
                                     charList[i].CharName = msg.GetString();
                                     charList[i].WorldName = msg.GetString();
                                     charList[i].WorldIP = msg.GetUInt32();
+                                    charList[i].WorldIPString = IPBytesToString(BitConverter.GetBytes(charList[i].WorldIP),0);
                                     charList[i].WorldPort = msg.GetUInt16();
                                 }
-                                //socket.BeginDisconnect(true, (AsyncCallback)LoginServerDisconnect, null);
+
+                                if (LoginServer_CharList != null)
+                                    LoginServer_CharList("Charlist received.");
+                                
+                                socket.Disconnect(true);
+                                if (Socket_Disconnected != null)
+                                    Socket_Disconnected("Charlist received.");
                                 return;
                             default:
                                 //Notify about an unknown message
-                                //socket.BeginDisconnect(true, (AsyncCallback)LoginServerDisconnect, null);
+                                if (LoginServer_UnknownMsg != null)
+                                    LoginServer_UnknownMsg(msg.Packet.ToHexString());
+
+                                socket.Disconnect(true);
+                                if (Socket_Disconnected != null)
+                                    Socket_Disconnected("Unknown Message.");
                                 break;
                         }
                     }
                 }
                 else //we didn't receive anything
-                {   
+                {
+                    if (LoginServer_ReceivedNothing != null)
+                        LoginServer_ReceivedNothing("Nothing received on LoginServerIndex=" + LoginServerIndex);
                     if(retry)
                     {
                         if(LoginServerIndex<MaxLoginServers-1)
                             TryLoginServer(LoginServerIndex++);
-                        else {}//tried all login servers and couldnt get character list, throw a notification
+                        else
+                        {
+                            if (LoginServer_CouldNotConnect != null)
+                                LoginServer_CouldNotConnect("dataLength<=0");
+
+                            socket.Disconnect(true);
+                            if (Socket_Disconnected != null)
+                                Socket_Disconnected("dataLength<=0");
+                        }
                     }
                 }
             }
             catch (Exception ex)
             {
             }
-        }        
+        }
+
 
         public class Signatures //4 bytes each file(tibia.dat,tibia.spr,tibia.pic)
         {
             public static byte[] Tibia840 = new byte[12]{0x7A,0x60,0x3D,0x49,
                                                         0x7C,0x4E,0x3D,0x49,
                                                         0x78,0x41,0x14,0x49};
+        }
+
+        public static string IPBytesToString(byte[] data, int index)
+        {
+            return "" + data[index] + "." + data[index + 1] + "." + data[index + 2] + "." + data[index + 3];
         }
 
 
