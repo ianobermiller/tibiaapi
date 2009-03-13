e316c73c4925e73b3ce6e311a09a068ed60fd573
brunodunbar <brunodunbar@ad62ca58-583e-0410-b638-772cefa7fa72>
2009-03-13 06:19:52 +0000

    Clean up the Xtea code.

--- trunk/tibiaapi/Packets/Xtea.cs
+++ trunk/tibiaapi/Packets/Xtea.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Tibia.Packets
+namespace TibiaEzBot.Packets
 {
     public static class Xtea
     {
-        public static bool XteaEncrypt(ref byte[] buffer, ref int length, int index, uint[] xteaKey)
+        public unsafe static bool XteaEncrypt(ref byte[] buffer, ref int length, int index, uint[] key)
         {
-            if (xteaKey == null)
+            if (key == null)
                 return false;
 
             int msgSize = length - index;
@@ -21,66 +21,55 @@ namespace Tibia.Packets
                 length = index + msgSize;
             }
 
-            for (int i = index; i < length; i += 8)
-                XTEAEncrypt(buffer, i, xteaKey);
+            fixed (byte* bufferPtr = buffer)
+            {
+                uint* words = (uint*)(bufferPtr + index);
+
+                for (int pos = 0; pos < msgSize / 4; pos += 2)
+                {
+                    uint x_sum = 0, x_delta = 0x9e3779b9, x_count = 32;
+
+                    while (x_count-- > 0)
+                    {
+                        words[pos] += (words[pos+1] << 4 ^ words[pos+1] >> 5) + words[pos+1] ^ x_sum
+                            + key[x_sum & 3];
+                        x_sum += x_delta;
+                        words[pos+1] += (words[pos] << 4 ^ words[pos] >> 5) + words[pos] ^ x_sum
+                            + key[x_sum >> 11 & 3];
+                    }
+                }
+            }
 
             return true;
         }
 
-        public static bool XteaDecrypt(ref byte[] buffer, ref int length, int index, uint[] xteaKey)
+        public unsafe static bool XteaDecrypt(ref byte[] buffer, ref int length, int index, uint[] key)
         {
-            if (length <= index || (length - index) % 8 > 0 || xteaKey == null)
+            if (length <= index || (length - index) % 8 > 0 || key == null)
                 return false;
 
-            for (int i = index; i < length; i += 8)
-                XTEADecrypt(buffer, i, xteaKey);
+            fixed (byte* bufferPtr = buffer)
+            {
+                uint* words = (uint*)(bufferPtr + index);
+                int msgSize = length - index;
 
-            int decrpytMsgLen = (int)BitConverter.ToUInt16(buffer, index) + 2;
-            length = decrpytMsgLen + index;
+                for (int pos = 0; pos < msgSize / 4; pos += 2)
+                {
+                    uint x_count = 32, x_sum = 0xC6EF3720, x_delta = 0x9E3779B9;
 
+                    while (x_count-- > 0)
+                    {
+                        words[pos + 1] -= (words[pos] << 4 ^ words[pos] >> 5) + words[pos] ^ x_sum
+                            + key[x_sum >> 11 & 3];
+                        x_sum -= x_delta;
+                        words[pos] -= (words[pos + 1] << 4 ^ words[pos + 1] >> 5) + words[pos + 1] ^ x_sum
+                            + key[x_sum & 3];
+                    }
+                }
+            }
+
+            length = (int)(BitConverter.ToUInt16(buffer, index) + 2 + index);
             return true;
-        }
-
-        private unsafe static void XTEAEncrypt(byte[] buffer, int offset, uint[] key)
-        {
-            // defintions that are used to create the cipher text
-            uint x_sum = 0, x_delta = 0x9e3779b9, x_count = 32;
-
-            // main part of the XTEA Cipher
-            fixed (byte* bufferPtr = buffer)
-            {
-                uint* words = (uint*)(bufferPtr + offset);
-
-                while (x_count-- > 0)
-                {
-                    words[0] += (words[1] << 4 ^ words[1] >> 5) + words[1] ^ x_sum
-                        + key[x_sum & 3];
-                    x_sum += x_delta;
-                    words[1] += (words[0] << 4 ^ words[0] >> 5) + words[0] ^ x_sum
-                        + key[x_sum >> 11 & 3];
-                }
-            }
-        }
-
-        private unsafe static void XTEADecrypt(byte[] buffer, int offset, uint[] key)
-        {
-            // defintions that are used to restore the plaintext text
-            uint x_count = 32, x_sum = 0xC6EF3720, x_delta = 0x9E3779B9;
-
-            fixed (byte* bufferPtr = buffer)
-            {
-                uint* words = (uint*)(bufferPtr + offset);
-
-                // main part of the XTEA Cipher
-                while (x_count-- > 0)
-                {
-                    words[1] -= (words[0] << 4 ^ words[0] >> 5) + words[0] ^ x_sum
-                        + key[x_sum >> 11 & 3];
-                    x_sum -= x_delta;
-                    words[0] -= (words[1] << 4 ^ words[1] >> 5) + words[1] ^ x_sum
-                        + key[x_sum & 3];
-                }
-            }
         }
     }
 }
