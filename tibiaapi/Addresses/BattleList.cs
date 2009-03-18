7746cdb87cbb40ecaa0b88ca4a4c58b6c3b9338b
brunodunbar <brunodunbar@ad62ca58-583e-0410-b638-772cefa7fa72>
2009-03-18 17:23:48 +0000

    Updated some address to tibia 8.41

--- tibiaapi/Addresses/BattleList.cs
+++ tibiaapi/Addresses/BattleList.cs
@@ -20,7 +20,7 @@ namespace Tibia.Addresses
         /// <summary>
         /// Start of the battle list.
         /// </summary>
-        public static uint Start = 0x062BD90; //8.40 Player.Exp + 108;
+        public static uint Start = 0x62CD90; //8.41 Player.Exp + 108;
 
         /// <summary>
         /// End of the battle list.
