1460f2659cb6a722939a0ed3024217efd15cb8bf
joebingham07 <joebingham07@ad62ca58-583e-0410-b638-772cefa7fa72>
2009-09-12 06:14:04 +0000

    Please excuse my multiple commits. I'm really tired ha ha.

---
+++ trunk/tibiaapi/Packets/Pipes/UpdateSkinPacket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Tibia.Objects;
+using Tibia.Constants;
+using System.Drawing;
+
+namespace Tibia.Packets.Pipes
+{
+    public class UpdateSkinPacket : AddSkinPacket
+    {
+        public UpdateSkinPacket(Client client)
+            : base(client)
+        {
+            Type = PipePacketType.UpdateSkin;
+        }
+
+        public override bool ParseMessage(NetworkMessage msg, PacketDestination destination)
+        {
+            if (msg.GetByte() != (byte)PipePacketType.UpdateSkin)
+                return false;
+
+            Type = PipePacketType.UpdateSkin;
+            PosX = msg.GetUInt16();
+            PosY = msg.GetUInt16();
+            Width = msg.GetUInt16();
+            Height = msg.GetUInt16();
+            SkinId = msg.GetUInt32();
+
+            return true;
+        }
+
+        public static new bool Send(Objects.Client client, uint skinId, ushort posX, ushort posY, ushort width, ushort height)
+        {
+            UpdateSkinPacket p = new UpdateSkinPacket(client);
+
+            p.PosX = posX;
+            p.PosY = posY;
+            p.Width = width;
+            p.Height = height;
+            p.SkinId = skinId;
+
+            return p.Send();
+        }
+
+    }
+}
